06869bc768a63f08d80e57d1f023650052e53479
Roberto Seidenberg <roberto.giessler@googlemail.com>
2020-04-29 18:11:59 +0200

    Removes rumble

--- js/main.js
+++ js/main.js
@@ -1,18 +1,3 @@
 $(document).ready(function() {
-
-	// Init rumbling links
-	$('a').jrumble({
-		x: 5,
-		y: 5,
-		rotation: 1
-	});
-
-	// Links are rumbling on mouseover
-	$('a').hover(function(){
-		$(this).trigger('startRumble');
-	}, function(){
-		$(this).trigger('stopRumble');
-	});
-
 	confetti();
 });
